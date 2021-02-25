09ced15d0d3fc41b18c97033143ad69455a573b9
ngakan1025 <ngakankrisna2001@gmail.com>
2021-02-25 22:01:51 +0800

    Project Web

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Objek Wisata Pantai Lebih</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="container" style="background-color: darkseagreen; ">
+  <div class="page-header">
+    <h1>Objek Wisata Pantai Lebih</h1>
+</div>
+
+<div class="container">
+    <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+    <li><a data-toggle="tab" href="#menu1">Artikel</a></li>
+    <li><a data-toggle="tab" href="#menu2">Kuliner</a></li>
+    <li><a data-toggle="tab" href="#menu3">Wisata</a></li>
+    </ul>
+</div>
+
+<div class="container" style="width: 700px;">  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="media/Pantai-Dreamland.jpg" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="media/Objek-wisata-pantai-Lebih.jpg" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="media/ombak-7-meter-terjang-pantai-lebih-800-2019-06-20-125903_0.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<div class="container" style="background-color: cornsilk; float: left; width: 725px; height: 1450px; margin: 15px;">
+    <h1><small>Keindahan Pantai Lebih</small></h1>
+    <h3><small>Menikmati kawasan pesisir pantai Bali, anda bisa coba ketenangan di pantai Lebih, lokasinya di sebelah Selatan ibu kota Gianyar. Pasir pantainya berwarna hitam berkilau, terlihat beda dengan pesisir pantai bagian Selatan seperti Kuta, Dreamland ataupun Tanjung Benoa yang didominasi oleh panati berpasir putih. Walaupun pantai Lebih ini berpasir hitam, namun demikian pesonanya juga tidak bisa dipandang sebelah mata saja, hamparan pantainya membentang dari Timur ke Barat sekitar 1.8 km, air lautnya berwarna biru jernih di hadapan pantai di ujung Selatan terlihat dataran pulau Nusa Penida.</small></h3>
+    <img src="media/PantaiLebih.png" class="img-thumbnail" alt="Cinque Terre" width="608" height="472"> 
+    <h1><small>Lokasi Pantai Lebih</small></h1>
+    <h3><small>Jika anda tertarik untuk mengunjungi pantai yang memiliki pasir hitam legam ini maka anda bisa langsung datang ke desa lebih, tepatnya di kecamatan dan Kabupaten Gianyar Bali. Jika anda sedang mengunjungi beberapa tempat wisata di Gianyar seperti rekreasi Ayung rafting ataupun Goa gajah maka anda bisa mampir ke pantai ini. Selain itu, rute untuk menuju ke pantai ini sendiri cukup mudah dan bisa diakses melalui jalan raya By Pass Ida Bagus Mantra. Dengan rute yang berada di jalan raya inilah maka akan memudahkan Anda yang tidak membawa kendaraan pribadi. Sampai di Pantai Pandan langsung akan terpesona melihat keindahan pantai ini.</small></h3>
+    <h1><small>Kuliner Pantai Lebih</small></h1>
+    <h3><small>Ketika anda sudah sampai Ke kawasan pantai ini maka jangan sampai melewatkan untuk mencoba sajian makanan laut. Nelayan yang ada di kawasan ini sendiri biasanya memiliki tangkapan yang didominasi oleh ikan tuna, ikan marlin dan tenggiri. Di tempat ini Anda juga bisa mencoba sate luluh yang dijual di warung tidak jauh dari kawasan pantai.</small></h3>
+    <img src="media/sate.png" class="img-thumbnail" alt="Cinque Terre" width="608" height="472">
+</div>
+
+<div class="col-lg-4  "  style="background-color:  aliceblue; float: right; width: 350px; margin: 15px;"> 
+    <div  class="widget  ">
+        <h2>Terpopular</h2>
+        <ul  class="menusidebar  ">
+            <li><a  href="  ">Kondisi Terkini Nissa Sabyan</a></li>
+            <li><a  href="  ">Banjir di Jakarta Berstatus Siaga</a></li>
+            <li><a  href="  ">Rencana Pelantikan Gibran</a></li>
+            <li><a  href="  ">Penggunaan Tali Masker Dinilai Berbahaya</a></li>
+        </ul>
+
+    <h2>Leave a Reply</h2>
+  <form>
+    <div class="form-group">
+      <label for="usr">Username:</label>
+      <input type="text" class="form" id="usr">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form" id="pwd">
+    </div>
+    <form>
+    <div class="form-group">
+      <label for="comment">Comment:</label>
+      <textarea class="form" rows="5" id="comment"></textarea>
+    </div>
+  </form>
+    <p>copyright 2021</p>
+</div>
+
+</body>
+</html>
